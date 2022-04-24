f98d131cf1a530730e11f10e23c2a238bd40ee07
AP Hacker <aphacker@bhaJjaka>
2022-04-25 01:34:46 +0545

    custaddvech

--- AdminPanel/custaddveh.php
+++ AdminPanel/custaddveh.php
@@ -1,3 +1,24 @@
+<?php
+if(isset($_POST["submit"]))
+   {
+    $v_brand = $_post['v_brand1'];
+    $v_model = $_post['v_model1'];
+    $v_reg_num = $_post['v_reg_num1'];
+    $v_year = $_post['v_year1'];
+
+    $query1="INSERT INTO `user_vehicle_details` (`veh_brand`, `veh_model`, `veh_y_o_m`, `veh_reg_num`) VALUES ('$v_brand', '$v_model', '$v_year','$v_reg_num')";
+    
+    if(mysqli_query($conn,$query1))
+	{
+	   echo "<script>window.open('index.php?updated=Record Has Been Updated','_self')</script>";
+	}
+	else{
+		echo "<div class='alert alert-danger' role='alert'> <b> Error!!! </b> <br>This Number is already in use. Please check the number you are trying to update</div>";
+	echo $query1;
+	}
+}
+
+?>
 <html>
 <html>
 <head>
@@ -13,32 +34,32 @@
  
     <div class="container">
         <div class="title">Add Your New Two-Wheeler</div>
-        <form action="#">
+        <form action="#" method="POST">
 
             <div class="user-details">Type
                 <div class="user-details">
                 <div class="input-box">
                     <span class="details">Brand</span>
-                    <input class="text" placeholder="Enter your two wheeler brand" required>
+                    <input class="text" placeholder="Enter your two wheeler brand" name="v_brand1" required>
                 </div>
            
                 <div class="input-box">
                     <span class="details">Model</span>
-                    <input class="text" placeholder="Enter your two wheeler model" required>
+                    <input class="text" placeholder="Enter your two wheeler model" name="v_model1" required>
                 </div>
             
             <div class="input-box">
                 <span class="details">Vechile Registration Number</span>
-                <input class="text" placeholder="Enter your Vechile Registration Number" required>
+                <input class="text" placeholder="Enter your Vechile Registration Number" name="v_reg_num1" required>
             </div>
             
             <div class="input-box">
                 <span class="details">Year of Manufacture</span>
-                <input class="text" type="date" required>
+                <input class="text" type="date" name="v_year1" required>
             </div>
                     
             <div class="button">
-            <input type="submit" value="Add Now">
+            <input type="submit" name="submit" value="Add Now">
             </div>
         </div>
             </div>
